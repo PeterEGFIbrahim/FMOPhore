cf0870d7c0da89c319dfda4e9e6ee8f7d3132f87
PeterEGFIbrahim <132382197+PeterEGFIbrahim@users.noreply.github.com>
2025-01-09 13:05:39 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,30 @@
 from setuptools import setup, find_packages
 
+# Optional: Read from README.md
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="FMOPhore",
     version="0.1",
     author="Peter E.G.F. Ibrahim",
-    author_email="2448959@dundee.ac.uk - peteregfi@gmail.com",
+    author_email="2448959@dundee.ac.uk, peteregfi@gmail.com",
     description="FMOPhore for hotspot identification and classification",
-    url="https://github.com/PeterEGFIbrahim/FMOPhore.git", 
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PeterEGFIbrahim/FMOPhore",
     packages=find_packages(),
     install_requires=[
         "numpy",
         "tqdm",
         "timeout-decorator",
-        "argparse",
-    ], 
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0 license",
-        "Operating System :: Linux",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6", 
+    python_requires=">=3.6",
     entry_points={
         "console_scripts": [
             "fmophore=FMOPhore.FMOPhore_run:main",
